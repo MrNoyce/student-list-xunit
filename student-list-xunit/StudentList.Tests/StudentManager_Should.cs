920bc0f8bf9eb094b3dcb65c2793736af8dc8435
Bradley Noyce <bradley@iris.net>
2019-04-15 11:04:45 +0300

    adding files for student list

---
+++ student-list-xunit/StudentList.Tests/StudentManager_Should.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StudentList.Tests
+{
+    public class StudentManager_Should
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
