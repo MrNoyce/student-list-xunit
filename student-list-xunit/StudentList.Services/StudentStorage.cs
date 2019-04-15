cb95adb3c77e030d37539256c510e0d2efdffe74
Bradley Noyce <bradley@iris.net>
2019-04-15 17:24:11 +0300

    Changing Folder Struct

--- student-list-xunit/StudentList.Services/StudentStorage.cs
+++ student-list-xunit/StudentList.Services/StudentStorage.cs
@@ -1,14 +1,37 @@
 using System;
+using System.IO;
+using System.Linq;
 
 namespace StudentList.Services
 {
     public class StudentStorage
     {
+        public const string StudentList = "students.txt";
+
+        ///<summary>
+        ///Replaces tje contents of the student storage with the given string of content.
+        ///The Method will also update the timestamp in the student storage.
+        ///</summary>
+        ///<param name="content">The new content for the student storage</param>
         public virtual void UpdateStudentList(string content)
         {
-            throw new NotImplementedException("Tests needed");
+            var timestamp = String.Format("List last updated on {0}", DateTime.Now);
+
+            // The 'using' construct does the heavy lifting of flushing a stream
+            // and releasing system resources the stream was using.
+            using (var fileStream = new FileStream(StudentList,FileMode.Open))
+            using (var writer = new StreamWriter(fileStream))
+            {
+                writer.WriteLine(content);
+                writer.WriteLine(timestamp);
+            }
         }
 
+
+        ///<summary>
+        ///Reads student data from student storage
+        ///</summary>
+        ///<returns> A string of student names </returns>
         public virtual string LoadStudentList()
         {
             throw new NotImplementedException("Tests needed");
